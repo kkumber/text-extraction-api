7ca6538ee7ed46d9e7c78de0a86b28d4fe8cf262
Angel Bert Reyes <angelbertreyes28@gmail.com>
2025-06-11 20:25:31 +0800

    added pdf text extraction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pymupdf
 import docx
 import pytesseract as ocr
 import PIL
+import io
 
 app = FastAPI()
 
@@ -17,11 +18,12 @@ async def upload_document(file: list[UploadFile] = File(...)):
         'application/msword',
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
         'text/plain',
-        'image/*'
+        'image/jpeg',
+        'image/png'
     ]
     
     for uploadedFile in file:
-        content = await file.read()
+        content = await uploadedFile.read()
         mime_type = magic.from_buffer(content, mime=True)
 
         if mime_type not in allowed_docs:
@@ -29,39 +31,54 @@ async def upload_document(file: list[UploadFile] = File(...)):
                 status_code=400,
                 detail=f"Document type not allowed. Received: {mime_type}"
             )
+            
+        if (mime_type == allowed_docs[0]):
+            extractedText.append(extract_text_from_pdf(content))
 
-    
     return {
-        "filename": file.filename,
-        "document_type": mime_type
+        "extracted_text": '.'.join(extractedText)
     }
 
+# Extract text in docx file using pymupdf
 def extract_text_from_pdf(file):
-    doc = pymupdf.read(file)
+    doc = pymupdf.open(stream=file, filetype='pdf')
     fullText = []
 
     for page in doc:
-        text = page.get_text().encode('utf8')
-        text.append(text)
-    print('\n'.join(fullText))
+        # Process all images on the page at once
+        image_list = page.get_images()
+        for img in image_list:
+            xref = img[0]
+            img_data = doc.extract_image(xref)
+            img_bytes = img_data["image"]
+            fullText.append(extract_text_from_image(img_bytes))
+        fullText.append(page.get_text())
+    return '\n'.join(fullText)
 
+# Extract text in docx file using python-docx
 def extract_text_from_docx(file):
-    doc = docx.Document(file)
+    doc = docx.Document(io.BytesIO(file))
     fullText = []
 
     for para in doc.paragraphs:
         fullText.append(para.text)
     print('\n'.join(fullText))
 
-def extract_text_from_image(file):
-    # Custom config for OCR page segmentation modes and engine mode
+# Extract text in docx file using pytesseract
+def extract_text_from_image(img_bytes):
+    # config for ocr engine and psm
     ocrConfig = r'--psm 6 --oem 3'
-
-    output = ocr.image_to_string(PIL.Image.open(file), config=ocrConfig)
+    image = PIL.Image.open(io.BytesIO(img_bytes))
+    output = ocr.image_to_string(image, config=ocrConfig)
     return output
 
+# Extract text in docx file using idk yet
 def extract_text_from_video(file):
     #extraction logic
+    pass
 
+# Extract text in docx file using idk yet
 def extract_text_from_pptx(file):
     #extraction logic
+    pass
+
